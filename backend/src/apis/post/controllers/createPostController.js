619469bed3fac5c6b44c20278301b8b301efcd35
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-25 16:34:06 +0600

    add post create controller

---
+++ backend/src/apis/post/controllers/createPostController.js
@@ -0,0 +1,42 @@
+import { postModel } from "../../../models/postModel.js";
+import { userModel } from "../../../models/userModel.js";
+import inputCheck from "../../../utils/inputCheck.js";
+import serverHelper from "../../../utils/serverHelper.js";
+
+export default function createPostController(req, res, next) {
+  const userId = req.userId;
+
+  const postData = req.body;
+  const { title, des, tag } = postData;
+
+  const check = inputCheck([title, des, tag], next);
+  if (!check) return;
+
+  serverHelper(async () => {
+    const filter = {
+      _id: userId,
+      $or: [{ badge: "gold" }, { badge: "bronze", postCount: { $lt: 5 } }],
+    };
+
+    const userPostCountUpdate = await userModel.updateOne(filter, {
+      $inc: { postCount: 1 },
+    });
+
+    if (userPostCountUpdate.modifiedCount === 0) {
+      res.status(400).json({
+        success: false,
+        message: "You have limited post, please update your plan",
+      });
+      return;
+    }
+
+    const tags = tag.map((ele) => ele.toLowerCase());
+
+    await postModel.create({ title, des, tags, author: userId });
+
+    res.status(201).json({
+      success: true,
+      message: "Post is created",
+    });
+  }, next);
+}
