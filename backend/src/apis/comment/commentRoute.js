de6e647167c4e9223602a6aac7e70d1002daef34
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-25 20:24:34 +0600

    add get all comments of post controller

--- backend/src/apis/comment/commentRoute.js
+++ backend/src/apis/comment/commentRoute.js
@@ -4,12 +4,14 @@ import verifyToken from "../../middleware/verifyToken.js";
 import verifyUser from "../../middleware/verifyUser.js";
 // controllers
 import createCommentController from "./controllers/createCommentController.js";
+import getCommentController from "./controllers/getCommentController.js";
 
 const commentRoute = Router();
 
 commentRoute
   .route("/:postId")
   .all(verifyToken)
-  .post(verifyUser(["user", "admin"]), createCommentController);
+  .post(verifyUser(["user", "admin"]), createCommentController)
+  .get(getCommentController);
 
 export default commentRoute;
---
+++ backend/src/apis/comment/controllers/getCommentController.js
@@ -0,0 +1,20 @@
+import { commentModel } from "../../../models/postModel.js";
+import serverHelper from "../../../utils/serverHelper.js";
+
+export default function getCommentController(req, res, next) {
+  const postId = req.params.postId;
+
+  serverHelper(async () => {
+    const comments = await commentModel
+      .find({ post: postId }, { __v: 0 })
+      .populate({
+        path: "user",
+        select: ["userName", "userEmail", "userPhoto"],
+      });
+
+    res.status(200).send({
+      success: true,
+      data: comments,
+    });
+  }, next);
+}
