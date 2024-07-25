007a9cdb871c02b9949638ba9691d63a96c93ed1
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-26 02:28:42 +0600

    add tanstack query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/assets/styles/global.css";
-import AuthContextProvider from "./context/AuthContext";
+import AuthContextProvider from "@/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
