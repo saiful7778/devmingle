0501c8ae11ef3e99802b9906df1f922d5aaefaf3
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-26 18:09:43 +0600

    add keep react ui component library

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [keepPreset],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
