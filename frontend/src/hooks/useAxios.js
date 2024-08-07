46894256e80bf93ed719ad74601f0cab59df0d75
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-08-07 14:55:39 +0600

    add secure axios hook

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -1,6 +1,48 @@
 import axiosConfig from "@/lib/configs/axios.config";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import useAuth from "./useAuth";
 
 export function useAxios() {
   return useCallback(axiosConfig, []);
+}
+
+export function useAxiosSecure() {
+  const { user, userData, token, logout } = useAuth();
+  const axios = useAxios();
+
+  useEffect(() => {
+    axios.interceptors.request.use((config) => {
+      config.params = {
+        ...config.params,
+        email: user?.email,
+        userId: userData?._id,
+      };
+
+      config.headers["Authorization"] = token;
+      return config;
+    });
+  }, [axios, token, user?.email, userData?._id]);
+
+  useEffect(() => {
+    axios.interceptors.response.use(
+      (data) => {
+        return data;
+      },
+      async (err) => {
+        try {
+          const status = err.response.status;
+
+          if (status === 401 || status === 403) {
+            await logout();
+          }
+        } catch (err) {
+          if (err instanceof Error) {
+            throw new Error(err.message);
+          }
+        }
+      }
+    );
+  }, [axios, logout]);
+
+  return axios;
 }
